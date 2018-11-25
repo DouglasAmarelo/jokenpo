89a742d584ef6c4ffc4ef82ce11e7caf682341aa
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2018-11-25 08:49:32 -0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+const gulp          = require('gulp');
+const sass          = require('gulp-sass');
+const browserSync   = require('browser-sync').create();
+const webpack       = require('webpack');
+const webpackStream = require('webpack-stream');
+const wait          = require('gulp-wait2');
+const eslint = require('gulp-eslint');
+
+gulp.task('browserSync', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'dist'
+		},
+		notify: false
+	})
+});
+
+gulp.task('html', function () {
+	gulp.src('./src/*.html')
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('video', function () {
+	gulp.src('./src/videos/*')
+		.pipe(gulp.dest('./dist/videos/'));
+});
+
+gulp.task('image', function () {
+	gulp.src('./src/images/*')
+		.pipe(gulp.dest('./dist/images'));
+});
+
+gulp.task('script', function () {
+	gulp.src('src/scripts/*.js',)
+	.pipe(eslint())
+	.pipe(eslint.format())
+	.pipe(eslint.failAfterError())
+	.pipe(webpackStream({
+		mode: 'production',
+		output: { filename: 'app.js', },
+		module: {
+			rules: [{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules)/,
+				loader: 'babel-loader',
+					query: {
+						presets: [
+							['latest', { modules: false }],
+						],
+					},
+				}],
+			}
+		}), webpack)
+		.pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('sass', function() {
+	return gulp.src('./src/sass/style.scss')
+			.pipe(wait(300))
+			.pipe(sass({
+				includePaths: ['./src/sass/'],
+				outputStyle: 'compressed'
+			})
+			.on('error', sass.logError))
+			.pipe(gulp.dest('./dist/css'))
+			.pipe(browserSync.reload({
+				stream: true
+			}));
+});
+
+gulp.task('watch', ['script', 'sass', 'html', 'image'], function() {
+	gulp.watch('./src/sass/**/*', ['sass']);
+	gulp.watch('./src/scripts/**/*.js', ['script']).on('change', browserSync.reload);
+	gulp.watch('./src/*.html', ['html']).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['browserSync', 'watch']);
+
+gulp.task('build', ['script', 'sass', 'html', 'image']);
---
+++ src/scripts/app.js
@@ -0,0 +1,15 @@
+(function() {
+	'use strict'
+
+	// Object that contains all application
+	const app = {};
+
+	// Start the application
+	app.init = () => {
+		console.log('Initing...');
+	};
+
+	// Init
+	app.init();
+
+})();
